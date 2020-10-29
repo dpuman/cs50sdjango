934ea34886065603c49e453c66654f416521e33a
Dipu <dpumail.in@gmail.com>
2020-10-29 23:27:46 +0600

    done django lecture

--- lecture3/newyear/views.py
+++ lecture3/newyear/views.py
@@ -4,5 +4,5 @@ import datetime
 
 def index(request):
     now = datetime.datetime.now()
-    return render(request, 'newyear/index.html', {'date': True})
+    return render(request, 'newyear/index.html', {'date': now.date == 1 and now.month == 1})
 # now.date == 1 and now.month == 1
